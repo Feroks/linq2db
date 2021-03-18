451ff444a3485cbd99344882dae4312d58e5d57a
Shane Krueger <shane@acdmail.com>
2021-03-18 06:42:43 -0400

    Prevent errors within TempTable.Dispose (#2883)
    * No error within TempTable.Dispose
    
    * Update test to exclude LINQ service
    
    * Update test
    
    * Update CreateTable_NoDisposeErrorAsync
    
    * Update table name
    
    * Fix tests again

--- Source/LinqToDB/TempTable.cs
+++ Source/LinqToDB/TempTable.cs
@@ -578,13 +578,13 @@ namespace LinqToDB
 
 		public virtual void Dispose()
 		{
-			_table.DropTable();
+			_table.DropTable(throwExceptionIfNotExists: false);
 		}
 
 #if !NETFRAMEWORK
 		public ValueTask DisposeAsync()
 		{
-			return new ValueTask(_table.DropTableAsync());
+			return new ValueTask(_table.DropTableAsync(throwExceptionIfNotExists: false));
 		}
 #endif
 	}
--- Tests/Linq/Update/CreateTempTableTests.cs
+++ Tests/Linq/Update/CreateTempTableTests.cs
@@ -243,5 +243,31 @@ namespace Tests.xUpdate
 				Assert.AreEqual(false, tableExists);
 			}
 		}
+
+		[Test]
+		public void CreateTable_NoDisposeError([DataSources(false)] string context)
+		{
+			using var db = new TestDataConnection(context);
+			db.DropTable<int>("TempTable", throwExceptionIfNotExists: false);
+
+			var tempTable = db.CreateTempTable<IDTable>("TempTable");
+			var table2 = db.GetTable<IDTable>().TableOptions(TableOptions.IsTemporary).TableName("TempTable");
+			table2.Drop();
+			tempTable.Dispose();
+		}
+
+#if !NETFRAMEWORK
+		[Test]
+		public async Task CreateTable_NoDisposeErrorAsync([DataSources(false)] string context)
+		{
+			using var db = new TestDataConnection(context);
+			await db.DropTableAsync<int>("TempTable", throwExceptionIfNotExists: false);
+
+			var tempTable = await db.CreateTempTableAsync<IDTable>("TempTable");
+			var table2 = db.GetTable<IDTable>().TableOptions(TableOptions.IsTemporary).TableName("TempTable");
+			await table2.DropAsync();
+			await tempTable.DisposeAsync();
+		}
+#endif
 	}
 }
