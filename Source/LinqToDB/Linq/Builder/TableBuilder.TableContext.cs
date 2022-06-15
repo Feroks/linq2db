78eea2436afe30c15e55bd8574b3bc9e5a365f6d
MaceWindu <macewindu@users.noreply.github.com>
2022-06-15 23:30:09 +0300

    Cherry picked #3558

--- Source/LinqToDB/Linq/Builder/TableBuilder.TableContext.cs
+++ Source/LinqToDB/Linq/Builder/TableBuilder.TableContext.cs
@@ -1189,7 +1189,7 @@ namespace LinqToDB.Linq.Builder
 						}
 						else
 						{
-							var tableLevel  = FindContextExpression(expression, level, true, true)!;
+							var tableLevel  = FindContextExpression(expression, level, forceInner: false, throwExceptionForNull: true)!;
 
 							var result = tableLevel.Context.GetContext(tableLevel.CurrentExpression, tableLevel.CurrentLevel + 1, buildInfo);
 							if (result == null)
